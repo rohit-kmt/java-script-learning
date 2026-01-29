5ea027de0ca580d4c4be37b81dd845195ccd070d
Rohit Kumawat <rohitkumawat@Mac.lan>
2026-01-29 21:26:19 +0530

    learn about alert , console , prompt , confirm

---
+++ 54.js
@@ -0,0 +1,18 @@
+alert("Hello, World!");
+console.log("alert system is working great!");
+
+var a= prompt("Enter your name: ");
+console.log("usre's name is " +a);
+alert("welcome " + a )
+
+var b = confirm("are you sure you want to continue?");
+
+if(b)
+{
+    alert("you pressed ok");
+}
+else
+{
+    alert("you pressed cancel");
+}
+console.log("end of alert demo");
