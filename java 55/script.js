d94ecdeec9dce7593bb43770510e217473f5b474
Rohit Kumawat <rohitkumawat@Mac.lan>
2026-02-04 17:25:18 +0530

    variables and object intro

---
+++ java 55/script.js
@@ -0,0 +1,20 @@
+console.log("Hello, World!");
+var a = 5;
+var b = 10;
+console.log("Sum is :" + (a+b));
+
+{
+    let a = 20;
+    let b = 30;
+    console.log(a);
+    console.log(b);
+}
+console.log(a);
+console.log(b);
+
+let c = {
+    name:"rohit",
+    age: 25
+}
+console.log(c);
+console.log(c.name);
