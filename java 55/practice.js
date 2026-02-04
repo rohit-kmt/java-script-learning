f932bac8faa335b48a8d8d36af71d4c1afea2d64
Rohit Kumawat <rohitkumawat@Mac.lan>
2026-02-04 17:46:29 +0530

    practice.js

---
+++ java 55/practice.js
@@ -0,0 +1,15 @@
+let a1=43;
+let b1="one"
+console.log(a1+b1); 
+console.log(typeof(a1+b1));
+
+const a={
+    name:"rohit",
+}
+a.name=23
+a.age=26
+console.log(a);
+console.log(a.name);
+const b= 23;
+//b1=45;
+console.log(b);
