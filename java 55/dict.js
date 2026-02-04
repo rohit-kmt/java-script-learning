2f33efcebfb46f2feaf56415700ae9ca9ccd51d5
Rohit Kumawat <rohitkumawat@Mac.lan>
2026-02-04 19:08:01 +0530

    dictionary

---
+++ java 55/dict.js
@@ -0,0 +1,1 @@
+console.log("helllo this is new dict file");
---
+++ java_56/conditions..js
@@ -0,0 +1,14 @@
+console.log("helllo this is new thing");
+
+let a=10;
+
+// if(a==110)
+// {
+//     console.log("condition is true");
+// }
+// else
+// {
+//     console.log("condition is false");
+// }
+
+console.log(a===10 ? "TRUE" : "FALSE");
