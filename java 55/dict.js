bad4ac1c1c0fd1a4d9c02ee126b7aea4f33cb886
rohit-kmt <rohitkmt192006@gmail.com>
2026-02-04 19:12:34 +0530

    condition statements

--- java 55/dict.js
+++ java 55/dict.js
@@ -1,1 +1,10 @@
 console.log("helllo this is new dict file");
+
+let dict={
+    constant:'any thing whose value not changes in any scenerio',
+    variable:'any thing whose value changes in any scenerio',
+    function:'a block of code which performs a specific task',
+    array:'a collection of items stored at contiguous memory locations',
+    object:'a collection of properties, and a property is an association between a name (or key) and a value'
+}
+console.log(dict);
