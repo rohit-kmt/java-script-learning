21c64ec09ada98c2e451f4cf7ac1f865efefeef8
rohit-kmt <rohitkmt192006@gmail.com>
2026-02-05 18:00:41 +0530

    practice condition stat.

---
+++ java_56/practice_set/div.js
@@ -0,0 +1,19 @@
+// let a=35;
+// if(a%2 === 0 && a%3 === 0)
+//     {
+//         console.log("divisible by 2 and 3");
+// }
+// else
+//     {
+//         console.log("divisible not by 2 and 3");
+//     }
+
+let a=34;
+if(a%2 === 0 || a%3 === 0)
+    {
+        console.log("divisible by 2 or 3");
+}
+else
+    {
+        console.log("divisible not by 2 or 3");
+    }
---
+++ java_56/practice_set/switch.js
@@ -0,0 +1,19 @@
+var day ="monday";
+var day = "tuesday";
+
+switch(day){
+    case "monday":
+        console.log("day of mahadev");
+        break;
+    case "tuesday":
+        console.log("day of hanuman");
+        break;
+    case "wednesday":
+        console.log("day of budh");
+        break;
+    case "thursday":
+        console.log("day of guru");
+        break;
+    case "friday":
+        console.log("day of shukra");       
+}
