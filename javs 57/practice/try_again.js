275f832b1149f17a4d53d95e8daaa34d93e0cb6c
rohit-kmt <rohitkmt192006@gmail.com>
2026-02-06 21:04:24 +0530

    guess no. made with html

---
+++ javs 57/practice/try_again.js
@@ -0,0 +1,19 @@
+let a=24124
+
+while(a !=32)
+{
+    a= prompt("enter an number : ");
+    
+    if(a>32)
+    {
+        alert("An lesser no. TRY AGAIN");
+        
+    }
+    else if (a<32)
+    {
+        alert('An greater no. TRY AGAIN');
+
+    }
+
+}
+    
