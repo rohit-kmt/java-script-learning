16adba42c405b7fabfb0bea4850b531930385675
rohit-kmt <rohitkmt192006@gmail.com>
2026-02-06 19:49:01 +0530

    loops intro

---
+++ javs 57/for_loop.js
@@ -0,0 +1,37 @@
+// for(let i=0; i<5; i++)
+//  {
+//     console.log("Rohit kumawat " + i);
+//  }
+
+
+// const object = {
+//     name: "Rohit",
+//     age: 25,
+//     city: "Jaipur"
+// }
+// for (const key in object) {
+//     console.log(key);
+    
+    
+// }
+
+
+// const array = ["Rohit", "Kumawat", 25, "Jaipur"];
+// for (const value of array) {
+//     console.log(value);
+// }
+
+
+// for (const element of "rohit") {
+//    console.log(element);
+// }
+let i=0;
+while (i<5) {
+    console.log(i*i);
+    i++;
+}
+
+do {
+    console.log(i);
+    i++;
+} while (i>10);
