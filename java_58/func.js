f0efdf8ab6c304ca05542300a56b5a580ca81057
rohit-kmt <rohitkmt192006@gmail.com>
2026-02-06 21:43:55 +0530

    functons in java

---
+++ java_58/func.js
@@ -0,0 +1,14 @@
+// function sum(a,b){
+//     console.log(a+b);
+
+// }
+
+// sum(3,34);
+
+function mean (a,b,c,d,e)
+{
+    console.log("Mean of given numbers : " + ((a+b+c+d+e)/5));
+}
+
+mean(3,4,3,23,3);
+mean(1,2,3,4,5);
