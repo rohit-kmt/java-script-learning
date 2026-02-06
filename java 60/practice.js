6089c3c40916a2316e0f8c7dcc0182c5941ba619
rohit-kmt <rohitkmt192006@gmail.com>
2026-02-06 23:04:55 +0530

    string basic and it's fuctions

---
+++ java 60/practice.js
@@ -0,0 +1,4 @@
+let str="please give rs 1000";
+console.log(str.indexOf(1));
+console.log(str.slice(15));
+console.log(str.replace("a","tttt"));
---
+++ java 60/string.js
@@ -0,0 +1,6 @@
+let names= "rohit"
+let str=`hellloo this is "HI" from ${names} the king.. `
+// console.log(str);
+// console.log(str.length);
+console.log(str.toString);
+console.log(str);
