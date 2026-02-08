3427d4c96f66d09d867f8c9520d0a0b3101d713e
rohit-kmt <rohitkmt192006@gmail.com>
2026-02-08 12:24:50 +0530

    array basic loops properties

---
+++ java 63/arr-loops.js
@@ -0,0 +1,20 @@
+const a=[3,33,333,3333,33333];
+
+// for (let i = 0; i < a.length; i++) {
+//     const element = a[i];
+//     console.log(element);
+// }
+
+// a.forEach((value , index ,ar) => {
+//     console.log(value , index , ar);
+// });
+
+// for (const element of a) {
+//     console.log(element)
+// }
+
+
+// a.forEach((element,index,array) => {
+//     // a.push(element);
+//     console.log(index);   
+// });
