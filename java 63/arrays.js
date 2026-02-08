eb87795968bd38c1bc3c0067ae238140b950185d
rohit-kmt <rohitkmt192006@gmail.com>
2026-02-08 12:24:07 +0530

    arrays basic

---
+++ java 63/arrays.js
@@ -0,0 +1,24 @@
+// console.log(arr[3]);
+// arr[3]=3;
+// console.log(arr[3]);
+
+const arr=[3,5,6,2,5];
+const arr1=[5,6];
+
+
+// console.log(arr.join(" to "));
+// arr.pop();
+
+// arr.push(100);
+// arr.shift();
+// arr.unshift("Kumawat");
+
+// console.log(arr.concat(arr1));
+
+
+// console.log(arr.splice(2,3,100,1,1000));
+// arr.splice(0,2);
+// arr.splice(0,0,34,333);
+// console.log(arr.slice(0,1));
+arr.reverse();
+console.log(arr);
